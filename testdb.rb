c860e24e428d156639c1ca2e85d6300d692573a6
Jack Yang <jcyang75@hotmail.com>
2015-10-05 16:48:47 -0700

    Second commit

---
+++ testdb.rb
@@ -0,0 +1,15 @@
+require 'tiny_tds'
+require 'active_record1'
+require 'logger'
+
+ActiveRecord::Base.logger = Logger.new('debug.log')
+configuration = YAML::load(IO.read('database.yml'))
+ActiveRecord::Base.establish_connection(configuration['development'])
+
+class Product < ActiveRecord::Base
+ 
+ p = Product.new
+ p.name = "New book"
+ puts p.name # "New book"
+ p.save
+end
